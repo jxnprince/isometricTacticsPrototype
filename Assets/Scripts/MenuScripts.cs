0029db8e2f9f3cbd2c9296bd5ea293f605023d81
jxnprince <68714241+jxnprince@users.noreply.github.com>
2021-09-13 10:35:16 -0600

    Initial Commit

---
+++ Assets/Scripts/MenuScripts.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor; //Added dependency to access objects in the Unity editor
+
+public class MenuScripts
+{
+
+    [MenuItem("Tools/Assign Tile Material")] //Makes the script availible in the tools dropdown in Unity
+    public static void AssignTileMaterial() //MenuScripts programatically adds a material to all tiles on a game board
+    {
+        GameObject[] tiles = GameObject.FindGameObjectsWithTag("Tile");  //Grab all tiles from Unity and put them in an array
+        Material newMaterial = Resources.Load<Material>("Tile"); //Reference to the material to be applied
+
+        foreach (GameObject t in tiles)
+        {
+            t.GetComponent<Renderer>().material = newMaterial; //Assigns material to each element in the tiles array
+        }
+    }
+
+}
