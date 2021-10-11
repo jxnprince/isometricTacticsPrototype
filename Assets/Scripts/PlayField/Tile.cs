e7102c81538c1bb8f3792d0eaf2539f11fd475f2
jxnprince <68714241+jxnprince@users.noreply.github.com>
2021-10-11 14:04:41 -0600

    Working on board generation

--- Assets/Scripts/PlayField/Tile.cs
+++ Assets/Scripts/PlayField/Tile.cs
@@ -5,10 +5,9 @@ using UnityEngine;
 public class Tile : MonoBehaviour{
     GameObject cube;
 
-    void Start()
+    public void Initialize()
     {
-        GameObject cube = new GameObject();
-        cube.AddComponent<MeshFilter>();
-        cube.AddComponent<BoxCollider>();
+        GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);  //Create basic cube
+        cube.AddComponent<BoxCollider>();  //Add collision.  Maybe change later?
     }
 }
