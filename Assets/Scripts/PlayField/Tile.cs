e4ba7dd3d9c6037dc6c2de31a6f021146512c070
jxnprince <68714241+jxnprince@users.noreply.github.com>
2021-10-12 10:40:34 -0600

    Grids now alternate material for better visual seperation

--- Assets/Scripts/PlayField/Tile.cs
+++ Assets/Scripts/PlayField/Tile.cs
@@ -6,8 +6,18 @@ public class Tile : MonoBehaviour
 {
     [SerializeField] private Material _baseTexture, _offsetTexture;
     [SerializeField] private MeshRenderer _renderer;
+    [SerializeField] private GameObject _highlight;
     
     public void Init(bool isOffset){
         _renderer.material = isOffset ? _offsetTexture : _baseTexture;
     }
+
+    void OnMouseEnter(){
+        _highlight.SetActive(true);
+    }
+
+    void OnMouseExit(){
+        _highlight.SetActive(false);
+    }
+
 }
