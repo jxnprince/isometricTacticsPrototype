ce4f7b4b70c340d865ed02ab2078be4f5ae45758
Jackson Prince <68714241+jxnprince@users.noreply.github.com>
2021-10-12 12:58:15 -0600

    Update GridManager.cs

--- Assets/Scripts/PlayField/GridManager.cs
+++ Assets/Scripts/PlayField/GridManager.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GridManager : MonoBehaviour{
-    [SerializeField] private int _width, _height;
-    [SerializeField] private Tile _tilePrefab;
+    [SerializeField] private int _width, _height; //Total width and hieght for a square playfield
+    [SerializeField] private Tile _tilePrefab; //Selects a prefab tile in Unity
 
 
     void Start()
@@ -13,13 +13,13 @@ public class GridManager : MonoBehaviour{
     }
 
     void GenerateGrid(){
-        for (int x = 0; x < _width; x++){
-            for (int z = 0; z < _height; z++){
-                var spawnedTile = Instantiate(_tilePrefab, new Vector3(x, 0, z), Quaternion.identity);
-                spawnedTile.name = $"Tile ({x},{z})";
+        for (int x = 0; x < _width; x++){ // Iterate over height
+            for (int z = 0; z < _height; z++){ //Iterate over the width.  Must be z axis and not y.
+                var spawnedTile = Instantiate(_tilePrefab, new Vector3(x, 0, z), Quaternion.identity); //creates new tile at a specific Vector.  Y is always a default of 0 on single level terrain.
+                spawnedTile.name = $"Tile ({x},{z})"; //Names Game Object created in unity instance.
 
-            var isOffset = (x % 2 != z % 2);
-            spawnedTile.Init(isOffset);
+            var isOffset = (x % 2 != z % 2); //determines if it is an even or odd tile
+            spawnedTile.Init(isOffset); //Init property assigns textures and materials to tiles.
             }
         }
     }
